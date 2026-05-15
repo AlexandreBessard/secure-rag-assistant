058dd3cb34d8f7adb9ae106ae37a147953baf814
Alex <alexandre.bessard@gmail.com>
2026-05-15 19:04:25 +0200

    Init project

---
+++ backend/src/test/java/com/lexoft/rag/RagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexoft.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
